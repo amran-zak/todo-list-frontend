578734029e64c9f48fc370fea1f2ee278ac9227e
Amran_zak <80887236+amran-zak@users.noreply.github.com>
2024-11-07 17:51:56 +0100

    Query Devtools

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@
 "use client";
 import ToDoList from '@/components/ToDoList';
 import { ReactQueryProvider } from '../lib/react-query';
+import { useState } from 'react';
+import { QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const Board: React.FC = () => {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold mb-6 text-center text-gray-500">Tableau de tâches</h1>
-      <ReactQueryProvider><ToDoList /></ReactQueryProvider>
+      <ReactQueryProvider client={queryClient}><ToDoList /> <ReactQueryDevtools initialIsOpen={false} position="bottom-right" /></ReactQueryProvider>
     </div>
   );
 };
